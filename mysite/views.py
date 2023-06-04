2893b036c50b3d2923bb783a8de05de87f4d8b7c
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-06-04 14:23:08 -0700

    Add a 'Projects' page that displays a list of projects from the database

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
 
+from .models import Project
+
 
 class HomePage(generic.TemplateView):
     template_name = 'index.html'
@@ -8,3 +10,12 @@ class HomePage(generic.TemplateView):
 
 class AboutPage(generic.TemplateView):
     template_name = 'about.html'
+
+
+class ProjectList(generic.ListView):
+    queryset = Project.objects.get_queryset()
+    template_name = 'projects.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
