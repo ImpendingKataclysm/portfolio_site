9a23cd43a8488a50af7b7f9e04e20f6ffd3a0bcf
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-06-04 14:04:46 -0700

    Add a database model for a portfolio project

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=500)
+    description = models.CharField(max_length=2000)
+    image_url = models.CharField(max_length=100)
+    image_alt = models.CharField(max_length=500)
+    source_code_url = models.CharField(max_length=1000)
+    live_url = models.CharField(max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
